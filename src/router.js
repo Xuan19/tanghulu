b24d5950ce4f449e7abecb8999a3c20abb8729e9
Xuan WANG <xuan.wang@expaceo.com>
2022-11-28 17:56:17 +0100

    add router

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import{createRouter, createWebHistory} from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory(),
+    route:[
+        {path: '/', redirect: '/coaches'},
+        {path: '/coaches', component: null},
+        {path: '/coaches/:id', component: null, children:[
+            {path:'contact', component:null},
+        ]},
+        {path: '/register', component: null},
+        {path: '/:notFound(.*)', component: null},
+    ]
+})
+
+export default router;
