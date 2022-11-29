e99a9a18a50407dc775a1e8d6d438105e518dbd0
Xuan WANG <xuan.wang@expaceo.com>
2022-11-29 16:04:04 +0100

    add vues and create store

--- src/pages/coaches/Coaches.vue
+++ src/pages/coaches/Coaches.vue
@@ -3,10 +3,31 @@
         Filter
     </section>
     <section>
-        list of coaches
+        <div class="controls">
+          <button>Refresh</button>
+          <router-link to="/register">Register as Coach</router-link>
+        </div>
+        <ul v-if="hasCoaches">
+          <li v-for="coach in filteredCoaches" :key="coach.id">
+            {{coach.firstName}}
+          </li>
+        </ul>
+        <h3 v-else>No coaches found</h3>
     </section>
 </template>
 
+<script>
+export default {
+  computed : {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches(){
+      return this.$store.getters['coaches/hasCoaches']
+    }
+  }
+}
+</script>
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
 * {
---
+++ src/pages/requests/ContactCoach.vue
@@ -0,0 +1,3 @@
+<template>
+    contact the coach
+</template>
