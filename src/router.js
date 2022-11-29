510ef2749fef22289ef5fcc1924919aeab7b10dd
Xuan WANG <xuan.wang@expaceo.com>
2022-11-29 11:32:44 +0100

    register routes and add route page components

--- src/router.js
+++ src/router.js
@@ -1,15 +1,27 @@
 import{createRouter, createWebHistory} from 'vue-router';
+import Coach from './pages/coaches/Coach.vue';
+import Coaches from './pages/coaches/Coaches.vue';
+import CoachResgistation from './pages/coaches/CoachRegistation.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
 
 const router = createRouter({
     history: createWebHistory(),
-    route:[
+    routes:[
         {path: '/', redirect: '/coaches'},
-        {path: '/coaches', component: null},
-        {path: '/coaches/:id', component: null, children:[
-            {path:'contact', component:null},
-        ]},
-        {path: '/register', component: null},
-        {path: '/:notFound(.*)', component: null},
+        {path: '/coaches', component: Coaches},
+        {
+            path: '/coaches/:id', 
+            component: Coach, 
+            children:[
+                {path:'contact', component:ContactCoach},
+            ]
+        },
+        {path: '/register', component: CoachResgistation},
+        {path: '/requests', component: RequestsReceived},
+        {path: '/:notFound(.*)', component: NotFound},
     ]
 })
 
