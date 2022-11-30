662439d1b43c28b86de791fd190d01470985f501
Xuan WANG <xuan.wang@expaceo.com>
2022-11-30 10:38:39 +0100

    add coache detail component

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -16,7 +16,7 @@
     </header>
 </template>
 
-<style>
+<style scoped>
 header {
   width: 100%;
   height: 5rem;
--- src/router.js
+++ src/router.js
@@ -15,6 +15,7 @@ const router = createRouter({
         {
             path: '/coaches/:id', 
             component: Coach, 
+            props:true,
             children:[
                 {path:'contact', component:ContactCoach},
             ]
