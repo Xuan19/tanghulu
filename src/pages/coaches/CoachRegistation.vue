555aa3e81d0d774ca7fce6aaf18b4029ed548441
Xuan WANG <xuan.wang@expaceo.com>
2022-12-01 10:43:02 +0100

    add register form

---
+++ src/pages/coaches/CoachRegistation.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+        <base-card>
+            <coach-form></coach-form>
+        </base-card>
+
+    </section>
+</template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue'
+
+export default {
+    components: {
+        CoachForm
+    }
+}
+</script>
+
+<style>
+
+</style>
