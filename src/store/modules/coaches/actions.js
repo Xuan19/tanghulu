4de8ac6fc16088185c67fb2201af57ae32a26264
Xuan WANG <xuan.wang@expaceo.com>
2022-12-02 13:39:59 +0100

    store and fetch coach data in firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,14 +1,55 @@
 export default{
-    registerCoach(context,data){
+    async registerCoach(context,data){
+        const userId = context.rootGetters.userId;
         const coachData = {
-            id:'c3',
             firstName: data.first,
             lastName: data.last,
             description: data.desc,
             hourlyRate:data.rate,
             areas: data.areas,
         }
-        //console.log(coachData );
-        context.commit('registerCoach', coachData)
+        
+        const response = await fetch(`https://tanghulu-8d72e-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,{
+            method: 'PUT',
+            body : JSON.stringify(coachData)
+        })
+
+        if(!response.ok){
+            console.log('error')
+        }
+
+        context.commit('registerCoach', {
+            ...coachData,
+            id : userId
+        })
+    },
+    async loadCoaches(context){
+        const response = await fetch(
+            `https://tanghulu-8d72e-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
+            );
+        const responseDate = await response.json();
+
+        if(!response.ok){
+            //
+        }
+
+        const coaches = [];
+
+        for(const key in responseDate){
+            const coach = {
+                id:key,
+                firstName: responseDate[key].firstName,
+                lastName: responseDate[key].lastName,
+                description: responseDate[key].description,
+                hourlyRate:responseDate[key].hourlyRate,
+                areas: responseDate[key].areas,
+            }
+
+            coaches.push(coach)
+        }
+
+        context.commit('setCoaches', coaches)
+
+
     }
 };
