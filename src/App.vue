8f2b4f1d952a98e982fcd5d4ad9284a48e83a48f
Xuan WANG <xuan.wang@expaceo.com>
2022-12-02 17:33:56 +0100

    transition

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
     <Header />
-    <router-view></router-view>
+    <router-view v-slot="slotProps">
+      <transition name="route" mode="out-in">
+         <component :is ="slotProps.Component"></component>
+      </transition>
+    </router-view>
 </template>
 
 <script>
@@ -25,5 +29,28 @@ html {
 
 body {
   margin: 0;
+}
+
+.route-enter-from{
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.route-enter-to,
+.route-leave-from{
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.route-leave-to{
+  opacity: 1;
+  transform: translateY(30px);
+}
+
+.route-enter-active{
+  transition: all 0.1s ease-out;
+}
+.route-leave-active{
+  transition: all 0.1s ease-in;
 }
 </style>
